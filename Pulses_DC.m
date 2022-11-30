46337cc05dcdc89a3ef9c82ef69bad231c85d23e
celia <celia@localhost>
2022-11-30 08:02:38 -0500

    Working script to program Keithley to output pulses or DC through arbitrary waveforms

--- Pulses_DC.m
+++ Pulses_DC.m
@@ -1,5 +1,7 @@
 %% Initialize
 
+% Delete instrument objects that are still open 
+delete(instrfindall)
 % Create TCP/IP object 't'. Specify server machine and port number.
 t = tcpip('192.168.0.2', 1394)
 % Set size of receiving buffer, if needed.
@@ -13,54 +15,85 @@ fscanf(t); % Receive lines of data from server
 
 %% Program pulse burst
 
-fprintf(t, "sour:wave:abor")
-fprintf(t, "sour:curr:rang:auto on")
-fprintf(t, "curr:filt off")
+fprintf(t, 'sour:wave:abor')
+fprintf(t, 'sour:curr:rang 200e-6')
+fprintf(t, 'curr:filt off')
+
+fprintf(t, 'outp:ishield guard')
+fprintf(t, 'outp:ltearth on')
+
 % Cathodic first, two data points per phase, 21 for the interpulse gap
-fprintf(t, "sour:wave:arb:data -1,-1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0")
-fprintf(t, "sour:wave:func arb0")
+% fprintf(t, 'sour:wave:arb:data -1,-1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0')
+fprintf(t, 'sour:wave:arb:data -1, 0.3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0')
+fprintf(t, 'sour:wave:func arb0')
 % Compliance voltage (V)
-fprintf(t, "curr:comp 15")
+fprintf(t, 'curr:comp 50')
 % Current amplitude (A)
-fprintf(t, "sour:wave:ampl 100e-6")
+fprintf(t, 'sour:wave:ampl 300e-6')
 % How many cycles I want
-fprintf(t, "sour:wave:dur:cycl 3000")
+fprintf(t, 'sour:wave:dur:cycl 3000')
 % Alternative for defining duration: how long do I want it to last (s)
-fprintf(t, "sour:wave:dur:time 4") % duration for 4 seconds
+fprintf(t, 'sour:wave:dur:time 4') % duration for 4 seconds
 % Frequency of the waveform. I think of this as the resolution for the duration of
 % my waveform. So if it's 400 Hz, then 1/400 = 2500 us
 % Now, since I have 25 slots in my waveform, that means that each one is
 % 100 us long.
-fprintf(t, "sour:wave:freq 400")
+fprintf(t, 'sour:wave:freq 400')
+
+fprintf(t, 'output off')
+fprintf(t, 'sour:wave:arm')
+fprintf(t, 'sour:wave:init')
+
+% fprintf(t, 'system:key 8')
 
 
-fprintf(t, "output off")
-fprintf(t, "sour:wave:arm")
-fprintf(t, "sour:wave:init")
+%% Program DC 'long pulse'
 
-%% Program DC "long pulse"
-
-fprintf(t, "sour:wave:abor")
-fprintf(t, "sour:curr:rang:auto on")
-fprintf(t, "curr:filt off")
+fprintf(t, 'sour:wave:abor')
+fprintf(t, 'sour:curr:rang:auto on')
+fprintf(t, 'curr:filt off')
 % Cathodic first, two data points per phase, 21 for the interpulse gap
-fprintf(t, "sour:wave:arb:data -1,1")
-fprintf(t, "sour:wave:func arb0")
+fprintf(t, 'sour:wave:arb:data -1,1')
+fprintf(t, 'sour:wave:func arb0')
 % Compliance voltage (V)
-fprintf(t, "curr:comp 15")
+fprintf(t, 'curr:comp 15')
 % Current amplitude (A)
-fprintf(t, "sour:wave:ampl 100e-6")
+fprintf(t, 'sour:wave:ampl 100e-6')
 % How many cycles I want
-fprintf(t, "sour:wave:dur:cycl 1")
+fprintf(t, 'sour:wave:dur:cycl 1')
 % Alternative for defining duration: how long do I want it to last (s)
-% fprintf(t, "sour:wave:dur:time 4")
+fprintf(t, 'sour:wave:dur:time 40')
 % Frequency of the waveform. 
-fprintf(t, "sour:wave:freq 0.25") % having each phase for 2 s
+fprintf(t, 'sour:wave:freq 0.25') % having each phase for 2 s
 
 
-fprintf(t, "output off")
-fprintf(t, "sour:wave:arm")
-fprintf(t, "sour:wave:init")
+fprintf(t, 'output off')
+fprintf(t, 'sour:wave:arm')
+fprintf(t, 'sour:wave:init')
+
+%% Program DC 
+
+fprintf(t, 'sour:wave:abor')
+fprintf(t, 'sour:curr:rang:auto on')
+fprintf(t, 'curr:filt off')
+% Cathodic first, two data points per phase, 21 for the interpulse gap
+fprintf(t, 'sour:wave:arb:data -1')
+fprintf(t, 'sour:wave:func arb0')
+% Compliance voltage (V)
+fprintf(t, 'curr:comp 100')
+% Current amplitude (A)
+fprintf(t, 'sour:wave:ampl 100e-6')
+% How many cycles I want
+fprintf(t, 'sour:wave:dur:cycl 1')
+% Alternative for defining duration: how long do I want it to last (s)
+fprintf(t, 'sour:wave:dur:time 40')
+% Frequency of the waveform. 
+fprintf(t, 'sour:wave:freq 0.25') % having each phase for 2 s
+
+
+fprintf(t, 'output off')
+fprintf(t, 'sour:wave:arm')
+fprintf(t, 'sour:wave:init')
 
 %% Disconnecting
 
